923bedc9ecd295867504aa42d26b2e4af64dacdf
Jesse Mullan <jmullan@jpmullan.com>
2011-09-18 13:06:34 -0700

    First working version

--- update_playlists.sh
+++ update_playlists.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-$sql_dir=$1
-$mt_daapd_songs3=$2
-$mt_daapd_music_dir=$3
-exit;
+if [ $# -ne 3 ] ; then
+    echo "Usage: update_playlists.sh /path/to/your/sql/dir /path/to/mt-daapd-db-file /path/to/mt-daapd-music-dir"
+    exit 1
+fi
+if [ ! -e "$1" ] ; then
+    echo ${1}": File Not Found"
+    exit 1
+fi
+if [ ! -e "$2" ] ; then
+    echo ${2}": File Not Found"
+    exit 1
+fi
+if [ ! -e "$3" ] ; then
+    echo ${3}": File Not Found"
+    exit 1
+fi
+
+sql_dir=$1
+mt_daapd_songs3=$2
+mt_daapd_music_dir=$3
+
 cd /data/music/parseDaapd/sql_to_playlist
 for file in `ls -1 *.sql` ; do
     basename=`echo $file | sed 's/.*\///'`
